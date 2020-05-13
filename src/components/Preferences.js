6db4e0af88ea379c0187f44f842926e94493cc5e
Hallek <paha@MACEX314.fritz.box>
2020-05-13 18:16:50 +0200

    Add preference inputs (non-functional)

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -3,9 +3,21 @@ import { jsx } from "theme-ui";
 import "../style.css";
 
 const Preferences = () => {
+
   return (
     <h3 sx={{ color: "text" }}>
-      Preferences
+      <div className="preference">
+        <h3>Irrigation time:</h3>
+        <input sx={{ color: "text" }} type="number" defaultValue="23" />
+      </div>
+      <div className="preference">
+        <h3>Capacity Buffer:</h3>
+        <input sx={{ color: "text" }} type="number" defaultValue="13" />
+      </div>
+      <div className="preference">
+        <h3>Minimum irrigation time interval:</h3>
+        <input sx={{ color: "text" }} type="number" defaultValue="32" />
+      </div>
     </h3>
   );
 };
