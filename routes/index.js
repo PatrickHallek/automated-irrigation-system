cc65ede21988ba6959b689642363a71a8337b911
Patrick Hallek <Patrick.Hallek@EXXETA.com>
2020-05-20 17:22:07 +0200

    Add sensor route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const IrrigationController = require("../controller/irrigation-controller");
 const MeasurementController = require("../controller/measurement-controller");
 const PreferenceController = require("../controller/preference-controller");
+const SensorController = require("../controller/sensor-controller");
 
 /* GET home page. */
 router.get('/measurements/:filter', MeasurementController.getMeasurements);
 router.get('/irrigations/:filter', IrrigationController.getIrrigations);
+router.get('/sensors/irrigation', SensorController.irrigation);
 router.get('/preferences', PreferenceController.getPreferences);
 
 router.put('/preferences', PreferenceController.updatePreferences);
