854b877c3e6dd723b4fedff4513d380fd507c659
Patrick Hallek <Patrick.Hallek@EXXETA.com>
2020-05-20 20:58:29 +0200

    Formate preference service

--- services/preference-service.js
+++ services/preference-service.js
@@ -1,33 +1,27 @@
 const Preference = require('../models/preferences');
 
 exports.getPreferences = async () => {
-    const preference = await Preference.findOneAndUpdate(
-        {},
-        {
-            $setOnInsert: {
-                minIrrigationIntervalInMinutes: 15,
-                irrigationTimeInSeconds: 2,
-                capacityBuffer: 30,
-                capacityMeanBuffer: 20
-            }
-        },
-        {
-            returnOriginal: false,
-            upsert: true,
-        });
+    const preference = await Preference.findOneAndUpdate({}, {
+        $setOnInsert: {
+            minIrrigationIntervalInMinutes: 15,
+            irrigationTimeInSeconds: 2,
+            capacityBuffer: 30,
+            capacityMeanBuffer: 20
+        }
+    }, {
+        returnOriginal: false,
+        upsert: true,
+    });
     return preference
 }
 
 exports.updatePreferences = async (body) => {
-    const preference = await Preference.findOneAndUpdate(
-        {},
-        {
-            $set: body
-        },
-        {
-            returnOriginal: false,
-            upsert: true,
-            new: true
-        });
+    const preference = await Preference.findOneAndUpdate({}, {
+        $set: body
+    }, {
+        returnOriginal: false,
+        upsert: true,
+        new: true
+    });
     return preference
 }
