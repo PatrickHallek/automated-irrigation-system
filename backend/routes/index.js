d4b167677da83a9e1ec987e58fd698004e9e6910
Patrick Hallek <Patrick.Hallek@EXXETA.com>
2020-06-04 17:34:58 +0200

    Add measurement post limitation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 const router = express.Router();
 const IrrigationController = require("../controller/irrigation-controller");
 const MeasurementController = require("../controller/measurement-controller");
 const PreferenceController = require("../controller/preference-controller");
 const SensorController = require("../controller/sensor-controller");
+
+const measurementLimiter = rateLimit({
+    windowMs: 20000, // 1 hour window
+    max: 1, // start blocking after 5 requests
+    message: "Too many measurements this IP"
+});
 
 /* GET home page. */
 router.get('/measurements/:filter', MeasurementController.getMeasurements);
@@ -13,6 +20,6 @@ router.get('/preferences', PreferenceController.getPreferences);
 
 router.put('/preferences', PreferenceController.updatePreferences);
 
-router.post('/measurement', MeasurementController.setMeasurement);
+router.post('/measurement', measurementLimiter, MeasurementController.setMeasurement);
 
 module.exports = router;
