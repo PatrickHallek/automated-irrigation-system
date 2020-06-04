12bbdeb7e34551b18b5c220dd74e9933ec7cf1ce
Patrick Hallek <Patrick.Hallek@EXXETA.com>
2020-06-04 15:41:54 +0200

    Fix rpio import issue for raspberry environment

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -1,14 +1,13 @@
-// Load envs
+const rpio = require('rpio');
 const dotenv = require('dotenv');
 dotenv.config();
 
-if (process.env.DEVELOPMENT) {  const rpio = {} }
-else { const rpio = require('rpio') }
-
 exports.irrigate = async (irrigationTimeInSeconds) => {
     console.log("Start Irrigation...")
+    rpio.open(process.env.RELAY_PIN, rpio.OUTPUT, rpio.LOW);
     rpio.write(process.env.RELAY_PIN, rpio.HIGH);
     rpio.sleep(irrigationTimeInSeconds);
     rpio.write(process.env.RELAY_PIN, rpio.LOW);
+    rpio.close(process.env.RELAY_PIN);
     return "Success"
 }
