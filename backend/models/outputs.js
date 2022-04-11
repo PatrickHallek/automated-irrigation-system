c2843ea821dcc6adafbad126e33ea81e5bc299e2
rafaelmuylaert <russo.simpson@gmail.com>
2022-04-10 22:03:30 -0400

    Update outputs.js

--- backend/models/outputs.js
+++ backend/models/outputs.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const portSchema = new Schema({ port: Number, time: Number });
 const outputSchema = new Schema({
    outputSensor: {
         type: String,
         require: true
    },
   Porttimes: {
-    type: Map,
-    of: new Schema({
-      port: Number,
-      time: Number
-    })
+    type: [portSchema],
+    default: undefined
   }
 });
 module.exports = mongoose.model('outputs', outputSchema);
