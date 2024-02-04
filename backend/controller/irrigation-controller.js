311786b61e318103938fbafafe0945fa5d216e89
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 17:23:34 -0500

    Update irrigation-controller.js

--- backend/controller/irrigation-controller.js
+++ backend/controller/irrigation-controller.js
@@ -3,3 +3,9 @@ const irrigationService = require("../services/irrigation-service")
 exports.getIrrigations = async (req, res, next) => {
     res.json(await irrigationService.getIrrigations(req.params.sensorName));
 };
+
+exports.irrigate = async (req, res, next) => {
+    await irrigationService.startIrrigation(req.params.sensorName);
+    res.status(200);
+    res.json("Success");
+};
