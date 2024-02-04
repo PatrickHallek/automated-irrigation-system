c57fb4c2e18b4e56e2c637de58405eb9fcb12fc0
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 12:21:11 -0500

    Update Preferences.js

--- src/components/Preferences/Preferences.js
+++ src/components/Preferences/Preferences.js
@@ -3,7 +3,7 @@ import { jsx } from "theme-ui";
 import "./Preferences.css";
 import { useState, useEffect } from "react";
 
-var defaultproperties {
+const defaultproperties = {
             minIrrigationIntervalInMinutes: 15,
             irrigationTimeInSeconds: 10,
             capacityBuffer: 50,
