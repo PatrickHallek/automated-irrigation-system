d35fa733903b71a1a38461e0bbd9b4e49212889d
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 17:23:28 -0500

    Update sensor-controller.js

--- backend/controller/sensor-controller.js
+++ backend/controller/sensor-controller.js
@@ -1,5 +1,3 @@
-const irrigationService = require("../services/irrigation-service")
-const sensorService = require("../services/sensor-service")
 const preferenceService = require("../services/preference-service")
 const data1 = require('../models/measurements/daily-measurement')
 const data2 = require('../models/measurements/hourly-measurement')
@@ -7,23 +5,9 @@ const data3 = require('../models/measurements/minutely-measurement')
 const data4 = require('../models/measurements/secondly-measurement')
 const prefdata = require('../models/preferences')
 
+
 exports.getSensorNames = async (req, res, next) => {
-    res.json(await sensorService.getSensorNames())
-};
-
-exports.irrigation = async (req, res, next) => {
-    const preferences = await preferenceService.getPreference(req.params.sensorName);
-    const sensorResult = await sensorService.irrigate(preferences.irrigationTimeInSeconds, req.params.sensorName);
-    if (sensorResult) {
-        await irrigationService.setIrregation(null).then(() => {
-            res.status(200);
-            res.json("Success");
-        });
-    } else {
-        res.status(500);
-        res.json("Failed");
-    }
-
+    res.json(await await preferenceService.getPreferences())
 };
 
 exports.deletesensor = async (req, res, next) => {
