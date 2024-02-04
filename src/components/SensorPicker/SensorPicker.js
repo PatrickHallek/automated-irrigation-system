5a3f95b60f7825f4912a27b8bfe53bb40c128a75
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-03 21:43:19 -0500

    Update SensorPicker.js

--- src/components/SensorPicker/SensorPicker.js
+++ src/components/SensorPicker/SensorPicker.js
@@ -5,8 +5,8 @@ import "./SensorPicker.css";
 const SensorPicker = props => {
     return (
         <div className="sensorPicker " sx={{ display: props.sensors.length > 1 ? "flex" : "none"}}>
-            {props.sensors.map((sensor) => <div key={sensor} onClick={() => props.setSensorInFocus(sensor)}
-                sx={{ bg: sensor === props.sensorInFocus ? "var( --primary) !important" : "backgroundSecondary" }}
+            {props.sensors.map((sensor) => <div sensor={sensor.Nickname} key={sensor.sensorName} onClick={() => props.setSensorInFocus(sensor.sensorName)}
+                sx={{ bg: sensor.sensorName === props.sensorInFocus ? "var( --primary) !important" : "backgroundSecondary" }}
             className="card"><h4>{sensor}</h4></div>)}
         </div>
     );
