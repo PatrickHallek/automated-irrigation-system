ca68b52097fe95411210de5bb425adc521b860e4
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 16:07:50 -0500

    Update sensor-service.js

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -1,22 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const preferenceService = require("../services/preference-service")
-let rpio = {};
-if (!process.env.DEVELOPMENT) rpio = require('rpio');
 
-exports.irrigate = async (irrigationTimeInSeconds, sensorName) => {
-    const preferences = await preferenceService.getPreference(sensorName)
-    console.log("Start Irrigation...")
-    rpio.open(preferences.signalPin, rpio.OUTPUT, rpio.LOW);
-    rpio.write(preferences.signalPin, rpio.HIGH);
-    rpio.sleep(irrigationTimeInSeconds);
-    rpio.write(preferences.signalPin, rpio.LOW);
-    rpio.close(preferences.signalPin);
-    return "Success"
-}
 
-exports.getSensorNames = async () => {
-    const preferences = await preferenceService.getPreferences()
-    // return preferences.map(preference => preference.sensorName)
-    return preferences
-}
+
+
+
