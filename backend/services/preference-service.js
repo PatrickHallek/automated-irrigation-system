b7a0bce20eb0637ea1f1c8b4fe3034b10b766375
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 12:21:28 -0500

    Update preference-service.js

--- backend/services/preference-service.js
+++ backend/services/preference-service.js
@@ -1,7 +1,7 @@
 const Preference = require('../models/preferences');
 
 exports.getPreference = async (sensorName) => {
-    var defaultproperties {
+    const defaultproperties = {
             minIrrigationIntervalInMinutes: 15,
             irrigationTimeInSeconds: 10,
             capacityBuffer: 50,
