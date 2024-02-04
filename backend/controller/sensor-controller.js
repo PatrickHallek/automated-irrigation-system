b34d4ee27fb690193f69b0c58b889ee118ff5586
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 13:29:07 -0500

    Update sensor-controller.js

--- backend/controller/sensor-controller.js
+++ backend/controller/sensor-controller.js
@@ -1,6 +1,11 @@
 const irrigationService = require("../services/irrigation-service")
 const sensorService = require("../services/sensor-service")
 const preferenceService = require("../services/preference-service")
+const data1 = require('../models/measurements/daily-measurement')
+const data2 = require('../models/measurements/hourly-measurement')
+const data3 = require('../models/measurements/minutely-measurement')
+const data4 = require('../models/measurements/secondly-measurement')
+const prefdata = require('../models/preferences')
 
 exports.getSensorNames = async (req, res, next) => {
     res.json(await sensorService.getSensorNames())
@@ -19,4 +24,23 @@ exports.irrigation = async (req, res, next) => {
         res.json("Failed");
     }
 
+};
+
+exports.deletesensor = async (req, res, next) => {
+    await data1.deleteMany({ sensorName: req.params.sensorName });
+    await data2.deleteMany({ sensorName: req.params.sensorName });
+    await data3.deleteMany({ sensorName: req.params.sensorName });
+    await data4.deleteMany({ sensorName: req.params.sensorName });
+    await prefdata.deleteMany({ sensorName: req.params.sensorName });
+    res.status(200);
+    res.json("Success");
+};
+
+exports.deletedata = async (req, res, next) => {
+    await data1.deleteMany({ sensorName: req.params.sensorName });
+    await data2.deleteMany({ sensorName: req.params.sensorName });
+    await data3.deleteMany({ sensorName: req.params.sensorName });
+    await data4.deleteMany({ sensorName: req.params.sensorName });
+    res.status(200);
+    res.json("Success");
 };
