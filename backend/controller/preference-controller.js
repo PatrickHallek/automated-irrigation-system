54c11cc8c0849e2c459a5fb2bf9a4cdf841262a9
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-04 17:43:48 -0500

    Update preference-controller.js

--- backend/controller/preference-controller.js
+++ backend/controller/preference-controller.js
@@ -4,6 +4,10 @@ exports.getPreference = async (req, res, next) => {
     res.json(await preferenceService.getPreference(req.params.sensorName));
 };
 
+exports.getPreferences = async (req, res, next) => {
+    res.json(await preferenceService.getPreferences());
+};
+
 exports.updatePreferences = async (req, res, next) => {
     res.json(await preferenceService.updatePreferences(req.body, req.params.sensorName));
 };
