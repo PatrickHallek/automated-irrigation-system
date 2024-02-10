3ac49ece80f028a3d22669102f0b4cb67f991106
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-10 00:33:10 -0500

    Advertise and read MDNS

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -1,5 +1,20 @@
-
-
+// import the module
+const mdns = require('mdns');
+ 
+// advertise a http server on port 4321
+const ad = mdns.createAdvertisement(mdns.tcp('http'), 4321);
+ad.start();
+ 
+ 
+// discover all available service types
+const browser = mdns.browseThemAll(); // all_the_types is just another browser..
+browser.on('serviceUp', service => {
+    console.log("service up: ", service);
+  });
+  browser.on('serviceDown', service => {
+    console.log("service down: ", service);
+  });
+  browser.start();
 
 
 
