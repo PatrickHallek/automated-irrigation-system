30c24109fb84a09305a32a6f80a839b40d1699ae
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-10 00:46:16 -0500

    a

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -2,8 +2,8 @@
 const mdns = require('mdns');
  
 // advertise a http server on port 4321
-const ad = mdns.createAdvertisement(mdns.tcp('http'), 4321);
-ad.start();
+// const ad = mdns.createAdvertisement(mdns.tcp('http'), 4321);
+// ad.start();
  
  
 // discover all available service types
