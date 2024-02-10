a326fb512eaa61baded5c8bd775a292224bd9bed
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-10 01:53:55 -0500

    a

--- backend/services/sensor-service.js
+++ backend/services/sensor-service.js
@@ -7,18 +7,16 @@ const mdns = require('multicast-dns');
  
  
 // discover all available service types
-
+mdns.on('response', function(response) {
+    console.log('got a response packet:', response)
+  })
+  
+  mdns.on('query', function(query) {
+    console.log('got a query packet:', query)
+  })
 
   exports.getMDNS = () => {
-    mdns.on('response', function(response) {
-        console.log('got a response packet:', response)
-      })
-      
-      mdns.on('query', function(query) {
-        console.log('got a query packet:', query)
-      })
-      
-      // lets query for an A record for 'brunhilde.local'
+          // lets query for an A record for 'brunhilde.local'
       mdns.query({
         questions:[{
           name: 'brunhilde.local',
