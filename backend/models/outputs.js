cef8d235e155ce7180b189ea2f18edd3a174bf81
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-13 07:31:24 -0500

    Update outputs.js

--- backend/models/outputs.js
+++ backend/models/outputs.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const portSchema = new Schema({ port: Number, time: Number });
 const outputSchema = new Schema({
    outputSensor: {
         type: String,
